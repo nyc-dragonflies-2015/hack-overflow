263f03ce2db521bcc25ace6b019469fe0b7f1809
Taylor Hang <sixthand6th@gmail.com>
2015-07-10 10:21:18 -0400

    add question validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,8 @@ class Question < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :user, presence: true
+
 end
