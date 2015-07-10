532d5718786d88da029a7f326f24ec5809798d56
Denny Tek <tek.denny@gmail.com>
2015-07-10 10:20:43 -0400

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
 
+  resources :users
+  resources :questions
+  resources :answers
+  resources :comments
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create', as: 'create_session'
+  delete 'logout' => 'sessions#destroy'
+
+  root 'welcome#index'
+
 end
