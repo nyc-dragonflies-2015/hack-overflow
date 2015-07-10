e352cd7c2668b98b16a690009df71da5267ec260
Taylor Hang <sixthand6th@gmail.com>
2015-07-10 11:48:09 -0400

    add comment controller and form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,25 @@
 class CommentsController < ApplicationController
 
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      if @comment.commentable_type == 'Question'
+        @question = Question.find(@comment.commentable_id)
+        @question.comments << @comment.save
+        render json: @comment.to_json
+      else if @comment.commentable_type == 'Answer'
+        @answer = Answer.find(@comment.commentable_id)
+        @answer.comments << @comment.save
+        render json: @comment.to_json
+      else
+        redirect_to :back
+      end
+  end
 
+  private
 
+    def comment_params
+      params.require(:comment).permit(:body, :user_id, :commentable_type, :commentable_id)
+    end
 
 end
