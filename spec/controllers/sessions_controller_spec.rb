8859d542f3664c5016f0e1666909855fad09a8af
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-10 15:45:11 -0400

    create users controller test and test framework for controller suite

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,9 @@
+require 'rails_helper'
+
 describe SessionsController do
   context '#create' do
     it 'should login a registered user' do
+      
     end
 
     it 'should not login an unregistered user' do
