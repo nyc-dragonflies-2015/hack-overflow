a6a0b029d1e2931b43963fd0a71d7d8d041d3304
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-09 20:23:58 -0400

    create Comment model test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,23 +2,26 @@ require 'rails_helper'
 
 describe Comment do
   it 'should respond to User' do
-  end
-
-  it 'should respond to Question' do
+    expect(FactoryGirl.create(:comment)).to respond_to :user
   end
 
   it 'should respond to Commentable' do
+    expect(FactoryGirl.create(:comment)).to respond_to :commentable
   end
 
   it 'should respond to Tags' do
+    expect(FactoryGirl.create(:comment)).to respond_to :tags
   end
 
   it 'should not save without a valid user' do
+    expect(FactoryGirl.build(:comment, user_id: nil)).to_not be_valid
   end
 
   it 'should not save without a body' do
+    expect(FactoryGirl.build(:comment, body: nil)).to_not be_valid
   end
 
   it 'should save if valid params' do
+    expect(FactoryGirl.create(:comment)).to be_valid
   end
 end
