a9c8bad7153bdacb8c1dec71e97f16d9bbaefd7c
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-10 15:43:30 -0400

    add flash message for failed registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash[:alert] = @user.errors.full_messages
       render :'new'
     end
   end
