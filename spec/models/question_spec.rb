9331aca4732fd843dded6b42ea5a4f6d52118604
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-09 20:24:25 -0400

    create Question model test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
+
 describe Question do
   it 'should respond to User' do
+    expect(FactoryGirl.create(:question)).to respond_to :user
   end
 
   it 'should respond to Comments' do
+    expect(FactoryGirl.create(:question)).to respond_to :comments
   end
 
   it 'should respond to Answers' do
+    expect(FactoryGirl.create(:question)).to respond_to :answers
   end
 
   it 'should respond to Votes' do
+    expect(FactoryGirl.create(:question)).to respond_to :votes
   end
 
   it 'should not save without a valid user' do
+    expect(FactoryGirl.build(:question, user_id: nil)).to_not be_valid
   end
 
   it 'should not save without a title' do
+    expect(FactoryGirl.build(:question, title: nil)).to_not be_valid
   end
 
   it 'should not save without a body' do
+    expect(FactoryGirl.build(:question, body: nil)).to_not be_valid
   end
 
   it 'should respond to Tags' do
+    expect(FactoryGirl.create(:question)).to respond_to :tags
   end
 end
