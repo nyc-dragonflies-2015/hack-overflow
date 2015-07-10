033d2160421f855e3a44a6dd8d1976129392043c
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-10 13:05:59 -0400

    add basic skeleton for Rspec controller tests

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,12 @@
+describe SessionsController do
+  context '#create' do
+    it 'should login a registered user' do
+    end
+
+    it 'should not login an unregistered user' do
+    end
+
+    it 'should display error messages if the form is not properly filled out' do
+    end
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,21 @@
+describe UsersController do
+  context "#new" do
+    it 'should render a registration form to the visitor' do
+    end
+  end
+
+  context '#edit' do
+    it 'should allow the user to edit their account information' do
+    end
+  end
+
+  context '#update' do
+    it 'should save the changes of the user account to the database' do
+    end
+  end
+
+  context 'destroy' do
+    it 'should delete the users account' do
+    end
+  end
+end
