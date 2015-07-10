b3ec6ceb6433bd6bc2013d9dbaaeb1b92e848db5
JMC11 <JMC11614792@gmail.com>
2015-07-10 10:36:17 -0400

    added session methods to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,26 @@
 class SessionsController < ApplicationController
+  def new
+    @user = user.new
+  end
 
+  def create
+    @user = User.find_by(username: user_params[:username])
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :points)
+  end
 end
