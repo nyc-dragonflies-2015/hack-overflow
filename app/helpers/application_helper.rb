0ac88c6083e44ace0032a009ffe55c3c2a9d9b98
JMC11 <JMC11614792@gmail.com>
2015-07-10 17:23:59 -0400

    fixed user controller issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def logged_in?
-    !!session[:user_id]
+  def require_logged_in
+    redirect_to root_path unless is_authenticated?
+  end
+
+  def is_authenticated?
+    return !!session[:user_id]
+  end
+
+  def current_user
+    @user = User.find(session[:user_id])
   end
 end
