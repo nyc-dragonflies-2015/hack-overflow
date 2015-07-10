f087da4f78f20f5503478bc05acda721afdbe21a
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-09 21:13:39 -0400

    create seed file for database with Hipster Ipsum API call :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'net/http'
+require 'json'
+require 'faker'
+require 'pry'
+
+def get_paragraph
+  uri = URI('http://hipsterjesus.com/api/?html=false')
+  response = Net::HTTP.get_response(uri)
+  binding.pry
+  body = JSON.parse(response.body)["text"]
+  title = body.split(" ").first(8).join(" ")
+  return [title, body]
+end
+
+10.times do
+  user = User.create(username: Faker::Name.name, email: Faker::Internet.email, password: "password")
+  paragraph = get_paragraph
+  user.questions << Question.create(title: paragraph[0], body: paragraph[1], view_count: 0 )
+end
+
+User.all.each_with_index do |user, index|
+  question = Question.all.sample
+  answer = Answer.create(question_id: question.id, body: "hello", user_id: User.first.id)
+  user.comments << Comment.create(commentable_type: "Question", commentable_id: question.id, body: Faker::Lorem.sentence)
+  user.answers << Answer.create(body: Faker::Lorem.paragraph)
+end
