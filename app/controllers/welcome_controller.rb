c77e94422f62994d733cced377fefb2fb7acacc1
JMC11 <JMC11614792@gmail.com>
2015-07-10 17:40:09 -0400

    landing page roughdraft

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
- def index
-  @user = User.new
-  @questions = Question.all
-  @questions = @questions.created_at.order("created_at DESC")
+  def index
+    @user = User.new
+    @questions = Question.all.order("created_at DESC")
   end
 end
