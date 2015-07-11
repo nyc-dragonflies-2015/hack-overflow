35ef588a9113ead106d8d0f5ebed438bea4864f5
Taylor Hang <sixthand6th@gmail.com>
2015-07-11 12:49:52 -0400

    get working ajax question comment going

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,18 +3,16 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
-    # if @comment.save
     if @comment.commentable_type == 'Question'
       @question = Question.find(@comment.commentable_id)
-      @comment.question_id = @question.id
-      @question.comments << @comment.save
-      render json: @comment.to_json
+      username = User.find(current_user.id).username
+      data = {comment: @comment, username: username}
     elsif @comment.commentable_type == 'Answer'
       @answer = Answer.find(@comment.commentable_id)
       @comment.question_id = @question.id
     end
     if @comment.save
-      render json: @comment.to_json
+      render json: data.to_json
     else
       redirect_to :back
     end
