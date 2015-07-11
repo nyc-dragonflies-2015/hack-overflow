06dda5456d66214cdb0169dcc747257f63d32a7d
JMC11 <JMC11614792@gmail.com>
2015-07-11 14:38:58 -0400

    fixed duplicate helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,30 +7,6 @@ module ApplicationHelper
     return !!session[:user_id]
   end
 
-  def current_user
-    @user = User.find(session[:user_id])
-  end
-
-  def authenticate_user!(owner_id)
-    if current_user != owner_id
-      redirect_to root_path, notice: "Access denied"
-    end
-  end
-
-  def signup_redirect
-    if !current_user
-      redirect_to new_user_path
-    end
-  end
-
-  def current_user
-    if session[:user_id]
-      return User.find(session[:user_id])
-    else
-      return nil
-    end
-  end
-
   def authenticate_user!(owner_id)
     if current_user != owner_id
       redirect_to root_path, notice: "Access denied"
