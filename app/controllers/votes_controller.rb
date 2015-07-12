8b5bcb0b6152ce2c60f734173e62bee70087b78a
Denny Tek <tek.denny@gmail.com>
2015-07-12 10:33:08 -0400

    Delete separate get-vote-value method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,16 +9,12 @@ class VotesController < ApplicationController
     @vote.user_id = current_user.id
       if @vote.voteable_type == 'Question'
         @question = Question.find(@vote.voteable_id)
-     # get_vote_value
       elsif @vote.voteable_type == 'Answer'
         @answer = Answer.find(@vote.voteable_id)
-     # get_vote_value
       elsif @vote.voteable_type == 'Comment'
         @comment = Comment.find(@vote.voteable_id)
-     # get_vote_value
       end
       if @vote.save
-        # redirect_to question_path(@vote.voteable_id)
         render json: @vote.to_json
       else
        ## redirect
@@ -46,13 +42,5 @@ class VotesController < ApplicationController
     def vote_params
       params.require(:vote).permit(:value, :user_id, :voteable_type, :voteable_id)
     end
-
-    # def get_vote_value
-    #   if params[:commit] == 'up'
-    #     @vote[:value] = 1
-    #   elsif params[:commit] == 'down'
-    #     @vote[:value] = -1
-    #   end
-    # end
 
 end
