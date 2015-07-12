c4c3ed9efea76621d87c65b64c4fa8ae7d7283a0
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-12 12:08:02 -0400

    complete spec for welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,13 @@
 describe WelcomeController do
   context "#index" do
     it 'should render all the questions to the visitor' do
+      get :index
+      expect(assigns(:questions)).to be_a ActiveRecord::Relation
+    end
+
+    it 'should render a form for a new user' do
+      get :index
+      expect(assigns(:user)).to be_a User
     end
   end
 end
