875fd890516ede9238d16451b7f773559e373b2f
JMC11 <JMC11614792@gmail.com>
2015-07-12 12:48:30 -0400

    fixed user dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :questions
-  has_many :answers
-  has_many :comments
-  has_many :votes
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   validates :username, :email, uniqueness: true, presence: true, length: {maximum: 20}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
