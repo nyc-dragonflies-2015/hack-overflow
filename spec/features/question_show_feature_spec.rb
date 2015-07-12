2c6cf21c283b53fbc7031b6576ce404b1be2076c
tgoldenberg <thomasagoldenberg@gmail.com>
2015-07-12 17:59:46 -0400

    make integration tests work for entire test suite

--- spec/features/question_show_feature_spec.rb
+++ spec/features/question_show_feature_spec.rb
@@ -12,48 +12,51 @@ end
 feature 'question#show' do
   scenario 'visit a question' do
     login(1)
-    find('a[href="/questions/1"]').click
+    id = Question.first.id
+    find('a[href="/questions/'+ id.to_s + '"]').click
     expect(page).to have_content 'Add Comment'
   end
 
   scenario 'add a comment for a question' do
     login(2)
-    find('a[href="/questions/7"]').click
+    id = Question.first.id
+    find('a[href="/questions/'+ id.to_s + '"]').click
     find('#question-comment-button').click
     find('#comment_body', :visible => false).set("Nice question!")
     find('#question_comment_submit', :visible => false).click
-    visit '/questions/7'
+    visit '/questions/' + id.to_s
     expect(page).to have_content 'Nice question!'
   end
 
   scenario 'add an answer for a question' do
     login(3)
-    find('a[href="/questions/13"]').click
+    id = Question.first.id
+    find('a[href="/questions/'+ id.to_s + '"]').click
     find('#question-comment-button').click
     find('#comment_body', :visible => false).set("Nice question!")
     find('#question_comment_submit', :visible => false).click
-    visit '/questions/13'
+    visit '/questions/' + id.to_s
     find('#answer-button').click
     find('#answer_body', :visible => false).set("This is my answer")
     save_and_open_page
     find('#answer-submit', :visible => false).click
-    visit '/questions/13'
+    visit '/questions/' + id.to_s
     save_and_open_page
     expect(page).to have_content 'This is my answer'
   end
 
   scenario 'add a comment for an answer' do
     login(4)
-    find('a[href="/questions/19"]').click
+    id = Question.first.id
+    find('a[href="/questions/'+ id.to_s + '"]').click
     find('#question-comment-button').click
     find('#answer-button').click
     find('#answer_body', :visible => false).set("This is my answer")
     find('#answer-submit', :visible => false).click
-    visit '/questions/19'
-    find('#answer-comment-button-2').click
+    visit '/questions/' + id.to_s
     find('.answer-comment-form #comment_body', :visible => false).set("Nice answer!")
     find('.answer-comment-form input[type=submit]', :visible => false).click
-    visit '/questions/19'
+    visit '/questions/' + id.to_s
     save_and_open_page
     expect(page).to have_content 'Nice answer!'
   end
